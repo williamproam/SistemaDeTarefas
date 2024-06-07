6c914eb525a80e1a548ec919c0f5b2b91b18d107
Matheus <Matheus@DESKTOP-VRFS3EI>
2024-06-07 19:17:43 -0300

    Controllers - Data

---
+++ Data/SistemasTarefasDBConext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemasTarefasDBConext : DbContext
+    {
+        public SistemasTarefasDBConext(DbContextOptions<SistemasTarefasDBConext> options)
+            :base(options)
+        { 
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+    }
+}
