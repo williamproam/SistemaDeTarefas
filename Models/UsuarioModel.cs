f69ecabfd33f3f44be14c377eb24ddf5c3e4e14d
Samuel Pereira <samuel_pereira71@hotmail.com>
2024-06-07 20:07:08 -0300

    Fazendo a ligação ao banco de dados

--- Models/UsuarioModel.cs
+++ Models/UsuarioModel.cs
@@ -2,7 +2,7 @@
 {
     public class UsuarioModel
     {
-    public int Id {  get; set; }
+        public int Id {  get; set; }
         public string? Nome { get; set; }
         public string? Email { get; set; }
     }
