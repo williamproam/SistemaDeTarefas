abf8a92105f72ec5625d03bc9a2090a35727bb58
Matheus <Matheus@DESKTOP-VRFS3EI>
2024-06-07 18:03:32 -0300

    b

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace SistemaDeTarefas.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "palavra"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "b"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
